2599d0ca6d609617f1f8a2bbc48ef72eac0283d8
nachoo369 <nachoo369@users.noreply.github.com>
2023-09-08 12:30:28 -0300

    suma

---
+++ sumar.js
@@ -0,0 +1,9 @@
+// Definir dos números
+var numero1 = 5;
+var numero2 = 10;
+
+// Sumar los dos números
+var resultado = numero1 + numero2;
+
+// Mostrar el resultado en la consola
+document.write("La suma de " + numero1 + " y " + numero2 + " es igual a " + resultado);
