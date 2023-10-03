6ba537c00c8ae3ea3fff8533fd39c8345b98e7ca
nachoo369 <nachoo369@users.noreply.github.com>
2023-10-02 23:49:36 -0300

    Estudiando

--- pythonf.py
+++ pythonf.py
@@ -1,8 +1,22 @@
-def factorial(num):
-    print("Valor inicial:", num)
-    if num > 1:
-        num = num * factorial(num -1)
-    print("valor final: ", num)
-    return num
+import numpy as np
 
-print(factorial(5))
+# Pedir al usuario el número de filas y columnas de la matriz
+filas = int(input("Ingrese el número de filas: "))
+columnas = int(input("Ingrese el número de columnas: "))
+
+# Inicializar una matriz vacía
+matriz1 = []
+
+# Pedir al usuario que ingrese los elementos de la matriz fila por fila
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        elemento = float(input(f"Ingrese el elemento en la fila {i + 1}, columna {j + 1}: "))
+        fila.append(elemento)
+    matriz.append(fila)
+
+# Convertir la lista de listas en una matriz NumPy
+matriz = np.array(matriz)
+
+print("La matriz ingresada es:")
+print(matriz)
